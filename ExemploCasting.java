a2458c1d02fe535052683d356c7c8f57c5f35ae5
Charles Pereira <charlesmyller@hotmail.com>
2022-07-12 00:37:55 +0200

    Tarefa wrappers

---
+++ ExemploCasting.java
@@ -0,0 +1,17 @@
+package br.com.charlespereira;
+
+public class ExemploCasting {
+	public static void main(String args[]) {
+		
+		long numL = 5566546484l;
+		System.out.println(numL);
+		int numI = (int) numL;
+		System.out.println(numI);
+		
+		Long numII = numL;
+		System.out.println(numII);
+		
+		
+		
+	}
+}
